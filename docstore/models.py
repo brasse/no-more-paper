6069d0e708247ba69a19b09628f8be17488e9731
Mattias Svala <thebrasse@brasse.org>
2009-09-06 19:53:34 +0000

    Added NumberSequence model.

--- docstore/models.py
+++ docstore/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Document(models.Model):
     file_name = models.CharField(max_length=200)
@@ -7,3 +8,10 @@ class Document(models.Model):
 
     def __unicode__(self):
         return self.file_name
+
+class NumberSequence(models.Model):
+    user = models.OneToOneField(User)
+    next_free_number = models.IntegerField()
+
+    def __unicode__(self):
+        return 'next: %d' % self.next_free_number
